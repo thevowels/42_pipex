4ecc3e48cc09358b0d857800acaaa673f1fb9a63
Aung Phyo Htet <aphyo-ht@z1t2c4.42bangkok.com>
2025-10-16 01:44:12 +0700

    checking between heredoc style and normal operation

--- pipex.c
+++ pipex.c
@@ -6,11 +6,12 @@
 /*   By: aphyo-ht <aphyo-ht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 01:01:12 by aphyo-ht          #+#    #+#             */
-/*   Updated: 2025/10/16 01:15:39 by aphyo-ht         ###   ########.fr       */
+/*   Updated: 2025/10/16 01:42:55 by aphyo-ht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
 
 int ft_startswith(char *str, char *start)
 {
@@ -26,15 +27,52 @@ int ft_startswith(char *str, char *start)
 	return (1);
 }
 
+void	ft_putstr(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	write(1, s, i);
+}
+
+int		ft_strexact(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while(*s1 && *s2)
+	{
+		if(*s1 != *s2)
+			return (0);
+		s1++;
+		s2++;
+	}
+	if(*s1 || *s2)
+		return (0);
+	return (1);
+}
+
 int main(int argc, char **argv, char **envp)
 {
-	while(*envp)
+
+	if(argc < 4)
 	{
-		if(ft_startswith(*envp,"PATH"))
-		{
-			printf("%s\n", (*envp) + 5);
-		}
-		envp++;
+		ft_putstr("Usage: ./pipex file1 cmd1 cmd2 ... file2 ");
+		ft_putstr("|| ./pippex here_doc LIMITER cmd cmd1 ... file \n");
+		return (1);
 	}
+	if(ft_strexact(argv[1], "here_doc"))
+	{
+		ft_putstr("Heredoc style\n");
+		return(0);
+	}
+	else
+	{
+		ft_putstr("Normal one\n");
+		return (0);
+	}
+
 	return (0);
 }
