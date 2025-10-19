84ed3b2767b14ef1c03f63dc0cea298ffbcab293
Aung Phyo Htet <aphyo-ht@z2t17c2.42bangkok.com>
2025-10-20 01:18:34 +0700

    feat: 	check arguments. 	check file access of input file 	test for execve usage

--- pipex.c
+++ pipex.c
@@ -6,73 +6,46 @@
 /*   By: aphyo-ht <aphyo-ht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 01:01:12 by aphyo-ht          #+#    #+#             */
-/*   Updated: 2025/10/16 01:42:55 by aphyo-ht         ###   ########.fr       */
+/*   Updated: 2025/10/20 01:16:07 by aphyo-ht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
-int ft_startswith(char *str, char *start)
+void prog_end(char *err_message)
 {
-	while(*str && *start)
-	{
-		if(*str != *start)
-			return (0);
-		str++;
-		start++;
-	}
-	if(*start)
-		return (0);
-	return (1);
+	printf("%s\n", err_message);
+	exit(EXIT_FAILURE);
 }
 
-void	ft_putstr(char *s)
+void check_IO_perms(char *infile)
 {
-	size_t	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	write(1, s, i);
-}
-
-int		ft_strexact(char *s1, char *s2)
-{
-	int	i;
-
-	i = 0;
-	while(*s1 && *s2)
+	if(access(infile, F_OK | R_OK) == 0)
 	{
-		if(*s1 != *s2)
-			return (0);
-		s1++;
-		s2++;
-	}
-	if(*s1 || *s2)
-		return (0);
-	return (1);
-}
-
-int main(int argc, char **argv, char **envp)
-{
-
-	if(argc < 4)
-	{
-		ft_putstr("Usage: ./pipex file1 cmd1 cmd2 ... file2 ");
-		ft_putstr("|| ./pippex here_doc LIMITER cmd cmd1 ... file \n");
-		return (1);
-	}
-	if(ft_strexact(argv[1], "here_doc"))
-	{
-		ft_putstr("Heredoc style\n");
-		return(0);
+		printf("File exists.\n");
 	}
 	else
 	{
-		ft_putstr("Normal one\n");
-		return (0);
+		prog_end("Problem reading input file.");
 	}
+}
 
+int	main(int argc, char **argv, char **envp)
+{
+	char *cat_path = "/usr/bin/cat";
+	char *cat_argv[] = {"cat", "abc", NULL};
+
+	
+	if(argc != 5)
+		prog_end("Usage: ./pipex file1 cmd1 cmd2 file2");
+	check_IO_perms(argv[1]);
+	int pid;
+	
+	execve(cat_path, cat_argv, envp);
+	
+	printf("Executed\n");
 	return (0);
 }
